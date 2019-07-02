95e3faafcbf0e153811fe0fb63d54f268b8728c7
INFINITECAMPUS\devon.tuma <devon.tuma@infinitecampus.com>
2019-07-02 15:15:24 -0500

    Problem 2

---
+++ src/bin/p02.rs
@@ -0,0 +1,36 @@
+// This doesn't correspond to a problem, essentially just a template file
+
+use std::env;
+use std::str::FromStr;
+
+#[allow(dead_code)]
+fn get_arg <T: FromStr> (arg_num: usize) -> T {
+    let args: Vec<String> = env::args().collect();
+    if args.len() <= arg_num {
+        panic!("Not enough arguments, expected at least {}", arg_num);
+    }
+    return match (&args[arg_num]).parse() {
+        Ok(parsed_value) => parsed_value,
+        Err(_) => panic!("Could not parse argument: {}", &args[arg_num])
+    }
+}
+
+// Finds the sum of even fibonacci numbers less than the input value
+fn main() -> () {
+    let max = get_arg::<i64>(1);
+
+    let mut current = (1,1);
+    let result: i64 = std::iter::from_fn(move || {
+        if current.1 <= max {
+            current = (current.1, current.0 + current.1);
+            Some(current)
+        }
+        else {
+            None
+        }
+    }).map(|x| x.0)
+    .filter(|x| x % 2 == 0)
+    .sum();
+
+    println!("{}", result);
+}
